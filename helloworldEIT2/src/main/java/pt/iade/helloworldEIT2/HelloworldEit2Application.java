7836ded098c38b880135178aab91faeac4710452
Miguel Bugalho <miguel.bugalho@gmail.com>
2020-10-26 14:24:54 +0000

    Spring Boot base helloworld example

---
+++ helloworldEIT2/src/main/java/pt/iade/helloworldEIT2/HelloworldEit2Application.java
@@ -0,0 +1,13 @@
+package pt.iade.helloworldEIT2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloworldEit2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloworldEit2Application.class, args);
+	}
+
+}
---
+++ helloworldEIT2/src/test/java/pt/iade/helloworldEIT2/HelloworldEit2ApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.iade.helloworldEIT2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloworldEit2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
